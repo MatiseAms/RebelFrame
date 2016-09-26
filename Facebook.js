c13dd751b1cc814d649d421ecb1659fa985ffa29
Maurits Meester <maurits@matise.nl>
2016-09-26 14:48:11 +0200

    Revert "Merge branch 'master' of github.com:MatiseAms/RebelFrame"
    This reverts commit 2b22ad1bc7d37f768d533296916f9015f00d394d, reversing
    changes made to c43ef66ca668263b184aa20dadddbd4a4bdb9b17.

--- Facebook.js
+++ Facebook.js
@@ -76,6 +76,8 @@ var FB = {
 
 function _authorize(options) {
 	var callbackWrapper = function(evt) {
+		console.log('callback');
+
 		fbSDK.removeEventListener('login', callbackWrapper);
 
 		// Add network to event object
@@ -117,8 +119,6 @@ function _authorize(options) {
 		callbackWrapper({success: true});
 
 	} else {
-		// fix for facbeook login on device, logout first before authorizing
-		fbSDK.logout();
 		fbSDK.authorize();
 	}
 }
